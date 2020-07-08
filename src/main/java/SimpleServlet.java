14d4af80d448306072fa2a3b338dfbfa6f608fe0
Jordan_COUDOUENT <jordanc31@icloud.com>
2020-07-08 16:09:08 +0200

    parameter pseudo

--- src/main/java/SimpleServlet.java
+++ src/main/java/SimpleServlet.java
@@ -8,8 +8,8 @@ import java.io.PrintWriter;
 
 public class SimpleServlet extends HttpServlet {
 
-    private static final String TEST_MESSAGE = "SIMPLE SERVLET OK" ;
-    
+    private static final String paramPseudo = "Bonjour " ;
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -18,6 +18,8 @@ public class SimpleServlet extends HttpServlet {
 
         //Actual logic goes here.
         PrintWriter out = resp.getWriter();
-        out.println("<h1>" + TEST_MESSAGE + "</h1>");
+        //out.println("<h1>" + TEST_MESSAGE + "</h1>");
+        final String paramPseudo = req.getParameter("pseudo");
+        out.println("Bonjour "+ paramPseudo + " ! </br>");
     }
 }
